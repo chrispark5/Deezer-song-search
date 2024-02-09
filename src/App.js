6a0ac80b6d0400309b4217858f4785a499fa3ccd
park2625 <park2625@umn.edu>
2024-02-08 23:42:38 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
+import SongCard from "./SongCard";
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '70740620a0mshbf974c8cccddfa0p14e6cfjsn6d2fe68326f9',
+		'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
+	}
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    useEffect(()=> {
+        searchSongs('Rose Colored')
+
+    }, [])
+
+    const handleKeyPress = (event) => {
+           if (event.key === "Enter") {
+               event.preventDefault();
+               searchSongs(searchTerm);
+           }
+       };
+
+    const [songs,setSongs] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
+    const searchSongs = async (title) => {
+        const response = await fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${title}&limit=10`, options);
+        var res = await response.json()
+        setSongs(res.data)
+        console.log(res.data)
+    }
+    return ( 
+        <div className="App">
+            <h1 id="appName">Spotify Playlist Editor</h1>
+
+            <div className = "search">
+                <input id="searchBar"placeholder= "Search for songs" value = {searchTerm} onChange={(e)=>setSearchTerm(e.target.value)} onKeyDown={handleKeyPress}/> 
+                <button onClick={()=>{searchSongs(searchTerm)}} >
+                Search
+            </button>
+            </div>
+              {
+                songs?.length >0
+                    ? (
+                        <div className = "container">
+                            {songs.map((song)=> (
+                            <SongCard key = {song.id}song ={song}/>))}
+              </div>) : (
+                    <div className = "empty">
+                        <h2>No Songs founds</h2>
+                    </div>
+              )
+            }
+        </div>
+    );
 }
 
-export default App;
+export default App
---
+++ src/SongCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const SongCard = ({song}) => {
+    return( 
+        <div className="songCard">
+            <div>
+                <p>{song.year}</p>
+            </div>
+
+            <div>
+                {song && song.album && song.album.cover_medium && 
+                  <img className = "albumCover"src={song.album.cover_medium}/>
+                }
+               
+            </div>
+
+            <div>
+                <span>{song.type}</span>
+                <h3>{song.title}</h3>
+            </div>
+        </div>
+
+
+    )
+
+}
+
+
+export default SongCard
